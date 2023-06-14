2dda3914af4943c279c091c3462e316d31d80fbd
Art Yudin <art@roboticview.com>
2023-06-14 14:04:50 -0400

    server

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ file_path = (f'{cwd}/data/gas_sale.csv')
 def main():
     data = get_data(file_path)
     app = Dash(external_stylesheets=[dbc.themes.LUMEN])
-    # server = app.server
     app.title = 'Gas Prices'
     app.layout = create_layout(app, data)
+    server = app.server
     app.run_server(debug=True)
 
 
